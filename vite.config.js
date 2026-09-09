297dfb74eccebb6ad2ee9db8578d1530324e78fd
Pedro Rener <renerpedro@gmail.com>
2026-09-09 16:48:39 -0300

    feat(writeups): add terminal window component; polish footer and auto last-update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const d = new Date()
+const LAST_UPDATE =
+  `${String(d.getFullYear()).padStart(4, '0')}-` +
+  `${String(d.getMonth() + 1).padStart(2, '0')}-` +
+  `${String(d.getDate()).padStart(2, '0')}`
+
 export default defineConfig({
   plugins: [react()],
+  define: {
+    __LAST_UPDATE__: JSON.stringify(LAST_UPDATE),
+  },
 })
